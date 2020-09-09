0414604e77c28677a613665ad39fbac1e52164ff
Sergio <sergioa.orozcoo@gmail.com>
2020-09-08 20:40:50 -0500

    saludame2

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+app.get("/makers/:nombre", (req, res) => {
+  const { nombre } = req.params;
+  let template = "Hola ";
+  template += capitalize(nombre) + "!";
+  if (nombre) {
+    res.send(`<h1>${template}</h1>`);
+  }
+});
+
+app.listen(3000, () => console.log("listening on port 3000!"));
